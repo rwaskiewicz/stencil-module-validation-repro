803414ea64b60bf9aaa7f8427bfdcc47b7659373
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-08-02 14:01:16 -0400

    repro(config): remove unneeded output targets
    remove unneeded output targets to make running this reproduction faster
    through a debugger, as doing so minimizes the number of code paths the
    compiler needs to take

--- stencil.config.ts
+++ stencil.config.ts
@@ -4,18 +4,7 @@ export const config: Config = {
   namespace: 'component-warn',
   outputTargets: [
     {
-      type: 'dist',
-      esmLoaderPath: '../loader',
-    },
-    {
       type: 'dist-custom-elements',
-    },
-    {
-      type: 'docs-readme',
-    },
-    {
-      type: 'www',
-      serviceWorker: null, // disable service workers
     },
   ],
 };
